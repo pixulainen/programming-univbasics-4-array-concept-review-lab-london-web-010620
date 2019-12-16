c1866a63f124df42ce5735f38bc7b13ebe0372ae
George Apetrei <george.apetrei@gmail.com>
2019-12-16 14:58:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,11 @@
-
 def find_element_index(array, value_to_find)
-array.length.times{
+array.length.times do
   |i| if array[i] == value_to_find
   return i
+  else
+  return nil
+  end 
   end
-} 
 end
 
 def find_max_value(array)
